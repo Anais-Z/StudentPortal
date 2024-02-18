bc94c33b1d33a4a43a532514911c3ae77d9799a8
Anais Zulfequar <zulfeqan@sheridancollege.ca>
2024-02-18 05:31:50 -0500

    Changed the properties.

--- StudentPortal/Models/Course.cs
+++ StudentPortal/Models/Course.cs
@@ -2,5 +2,16 @@
 {
     public class Course
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CourseId { get; set; }
+
+        public string CourseDescription { get; set; }
+
+        public List<User> Students { get; set; }
+
+        public User Proffesor { get; set; }
     }
 }
--- StudentPortal/Models/User.cs
+++ StudentPortal/Models/User.cs
@@ -1,6 +1,19 @@
 ﻿namespace StudentPortal.Models
 {
-    public class Student
+    public class User
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Status { get; set; }
+
+        public List<Course> Courses { get; set; }
     }
 }
