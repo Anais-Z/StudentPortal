640ee3e3c295e8d530e2146c0d269e9533923872
Anais <anaiszed447@gmail.com>
2024-03-04 09:41:08 -0500

    Created the address interface methods

--- StudentPortal/Interfaces/IAddressRepository.cs
+++ StudentPortal/Interfaces/IAddressRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace StudentPortal.Interfaces
+﻿using StudentPortal.Models;
+
+namespace StudentPortal.Interfaces
 {
     public interface IAddressRepository
     {
+        ICollection<Address> GetAddresses();
+
+        Address GetAddressById(int id);
+
+        bool CreateAddress(Address address);
+
+        bool AddressExists(int id);
+
+        bool UpdateAddress(Address address);
+
+        bool DeleteAddress(Address address);
     }
 }
